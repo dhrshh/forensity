fedcda4184a01ca8df86522e4be665b493158d26
Dhara Shah <dhrshh@gmail.com>
2014-11-18 21:48:49 +0000

    Navigation bar, routes and home page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def search
+  end
+
+  def match
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+end
